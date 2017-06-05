f2e1814d6c4276eb579b2504b522131634797a31
wilczewskilukasz <wilczewskilukasz@gmail.com>
2017-06-06 00:27:39 +0200

    webapi
    000 - utworzenie podstawy api wraz z konfiguracja

---
+++ HRinfoAPI/HRinfoAPI/Controllers/ApiTestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HRinfoAPI.Controllers
+{
+    public class ApiTestController : ApiController
+    {
+        public string Test()
+        {
+            return "Test value.";
+        }
+
+        public int Test(int value)
+        {
+            return value;
+        }
+    }
+}
