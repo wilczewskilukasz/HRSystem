59652f8bae5a18efdffef2d7ab6e7b7b665a8b42
wilczewskilukasz <wilczewskilukasz@gmail.com>
2017-06-14 00:12:37 +0200

    api
    CalendarsController - to do...

--- HRinfoAPI/HRinfoAPI/Controllers/EmployeeController.cs
+++ HRinfoAPI/HRinfoAPI/Controllers/EmployeeController.cs
@@ -2,13 +2,10 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using System.Web.Http.Description;
 using HRinfoAPI.Models;
 using Microsoft.AspNet.Identity;
 
