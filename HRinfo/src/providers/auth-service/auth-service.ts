6791a95757461aaadb59748b5f2f5030db290241
wilczewskilukasz <wilczewskilukasz@gmail.com>
2017-06-22 19:41:27 +0200

    client

--- HRinfo/src/providers/auth-service/auth-service.ts
+++ HRinfo/src/providers/auth-service/auth-service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+﻿import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { AlertController } from 'ionic-angular';
